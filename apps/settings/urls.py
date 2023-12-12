352cc1c72dc02f30f9522cfc7981d42b7d20c69c
nurai003002 <nuraj9663@gmail.com>
2023-12-12 14:55:53 +0600

    change2

--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from apps.settings.views import index1,index2, index3
 
 urlpatterns = [
-    path('1', index1, name = 'index'),
-    path('2', index3, name= 'index3'),
+    path('blog-post.html', index1, name = 'index'),
+    path('blog.html', index3, name= 'index3'),
     path('', index2, name='index2')
-
 ]
